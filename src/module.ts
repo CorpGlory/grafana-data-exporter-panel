dad7d08d32ded24af81062c48d4fb671f24b2aa9
rozetko <rozetko@corpglory.com>
2019-10-21 13:59:01 +0300

    test

--- src/module.ts
+++ src/module.ts
@@ -5,6 +5,7 @@ import './css/panel.light.scss';
 import { PanelCtrl } from 'grafana/app/plugins/sdk';
 import { appEvents } from 'grafana/app/core/core';
 
+import url from 'url-parse';
 import _ from 'lodash';
 import $ from 'jquery';
 import moment from 'moment';
@@ -183,6 +184,34 @@ class Ctrl extends PanelCtrl {
 
   onRangeToChange() {
     this.rangeOverride.to = moment(this.rangeOverrideRaw.to).format();
+  }
+
+  normalizeUrl(inputUrl: string) {
+    if(!inputUrl) {
+      return inputUrl;
+    }
+    let urlObj = new url(inputUrl, {});
+    if(urlObj.protocol !== 'http:' && urlObj.protocol !== 'https:') {
+      inputUrl = `http://${inputUrl}`;
+      urlObj = new url(inputUrl, {});
+      console.log('No protocol provided in inputUrl -> inserting "http://"');
+    }
+    if(urlObj.slashes === false) {
+      urlObj = new url(`${urlObj.protocol}//${urlObj.pathname}`, {});
+      console.log('No slashes were provided after the protocol -> inserting slashes');
+    } 
+    if(urlObj.pathname.slice(-1) === '/') {
+      urlObj.pathname = urlObj.pathname.slice(0, -1);
+      console.log('Removing the slash at the end of inputUrl');
+    }
+    let finalUrl = `${urlObj.protocol}//${urlObj.hostname}`;
+    if(urlObj.port !== '') {
+      finalUrl = finalUrl + ':' + urlObj.port;
+    }
+    if(urlObj.pathname !== '') {
+      finalUrl = finalUrl + urlObj.pathname;
+    }
+    return finalUrl;
   }
 
   async export(panelId, target) {
